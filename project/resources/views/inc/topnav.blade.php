53756eb3a658715b3756eeab90818d90adbc937a
Shahriar-F <shahriarfahim20@gmail.com>
2022-03-14 02:57:03 +0600

    initial

---
+++ project/resources/views/inc/topnav.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand " href="/home"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/home">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="registration">Registration</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/login">Log In</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/logout">Log Out</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/profile">Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/orders">Orders</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/history">History</a>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
