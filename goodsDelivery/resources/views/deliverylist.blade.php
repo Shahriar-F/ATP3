89c42951f6d0524b84a9698db34f7ae15440cbad
Shahriar-F <shahriarfahim20@gmail.com>
2022-03-14 23:07:39 +0600

    project

---
+++ goodsDelivery/resources/views/deliverylist.blade.php
@@ -0,0 +1,9 @@
+<body>
+    <div class = "container bg-light">
+</body>
+
+@extends ('layout.app')
+@section('content')
+
+<h3>Delivery List </h3>
+@endsection
---
+++ goodsDelivery/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<body>
+    <div class = "container bg-light">
+</body>
+
+@extends ('layout.app')
+@section('content')
+
+<h3>Welcome </h3>
+@endsection
